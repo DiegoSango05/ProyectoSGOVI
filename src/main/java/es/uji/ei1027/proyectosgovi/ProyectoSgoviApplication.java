5ed1099c5af50d2df6c99631e2ee68f230357245
Diego <al435161@uji.es>
2026-03-03 09:53:29 +0100

    PROYECTO CREADO

---
+++ src/main/java/es/uji/ei1027/proyectosgovi/ProyectoSgoviApplication.java
@@ -0,0 +1,13 @@
+package es.uji.ei1027.proyectosgovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSgoviApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoSgoviApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/uji/ei1027/proyectosgovi/ProyectoSgoviApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uji.ei1027.proyectosgovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSgoviApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
